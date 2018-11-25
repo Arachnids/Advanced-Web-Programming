85acaf0705be128e522eb189b8824c38c38f3ce0
Jorge Soto <Jorgersotobravo@gmail.com>
2018-11-24 20:40:26 -0400

    Fix for heroku v3

--- config/db.js
+++ config/db.js
@@ -1,16 +1,25 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('formula1', 'postgres', '123', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-});
+const database = process.env.DATABASE_URL || 'formula1';
+let sequelize = '';
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(database);
+} else {
+  sequelize = new Sequelize('formula1', 'postgres', '123', {
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorsAliases: false,
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  });
+
+}
 
 module.exports.sequelize = sequelize;
 module.exports.Sequelize = Sequelize;
